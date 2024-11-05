77eca0aff0ba78cce3e8dadee0e88aab3f11d7d8
Shreyas Sulakhe <shreyassulakhe03@gmail.com>
2024-11-05 11:40:41 +0530

    Removed unnecessary files

--- Assignment2/Contactsapplication2/app/src/main/java/com/example/contacts_application2/Contact.kt
+++
@@ -1,8 +0,0 @@
-package com.example.contacts_application2
-import android.graphics.Bitmap
-
-data class Contact(
-    var name: String,
-    var phoneNumber: String,
-    var image: Bitmap? = null
-)
